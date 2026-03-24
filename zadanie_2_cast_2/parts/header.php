6769404a237da592c2aef0477dfd641195841d9d
Kristián Tóth <kikuskomikusko@gmail.com>
2026-03-24 18:29:29 +0100

    kaslem to

--- zadanie_2_cast_2/parts/header.php
+++ zadanie_2_cast_2/parts/header.php
@@ -1,7 +1,4 @@
 
-<link rel="stylesheet" href="/css/style.css">
-<link rel="stylesheet" href="/css/slider.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 <header class="container main-header">
         <div>
